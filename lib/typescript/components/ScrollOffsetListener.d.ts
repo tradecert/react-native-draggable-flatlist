4bf65681af50c817f405d76ebb03b8e92a5a2555
Harvey Appleton <info@harveyappleton.co.uk>
2025-10-22 14:39:25 +0100

    Prettier removed

--- lib/typescript/components/ScrollOffsetListener.d.ts
+++ lib/typescript/components/ScrollOffsetListener.d.ts
@@ -1,7 +1,7 @@
 import Animated from "react-native-reanimated";
 declare type Props = {
-  scrollOffset: Animated.SharedValue<number>;
-  onScrollOffsetChange: (offset: number) => void;
+    scrollOffset: Animated.SharedValue<number>;
+    onScrollOffsetChange: (offset: number) => void;
 };
-declare const _default: ({ scrollOffset, onScrollOffsetChange }: Props) => null;
+declare const _default: ({ scrollOffset, onScrollOffsetChange, }: Props) => null;
 export default _default;
--- lib/typescript/hooks/useOnCellActiveAnimation.d.ts
+++ lib/typescript/hooks/useOnCellActiveAnimation.d.ts
@@ -1,13 +1,11 @@
 import { WithSpringConfig } from "react-native-reanimated";
 declare type Params = {
-  animationConfig: Partial<WithSpringConfig>;
+    animationConfig: Partial<WithSpringConfig>;
 };
-export declare function useOnCellActiveAnimation({
-  animationConfig,
-}?: Params): {
-  isActive: boolean;
-  onActiveAnim: Readonly<{
-    value: 0 | 1;
-  }>;
+export declare function useOnCellActiveAnimation({ animationConfig }?: Params): {
+    isActive: boolean;
+    onActiveAnim: Readonly<{
+        value: 0 | 1;
+    }>;
 };
 export {};
